acb5bcd65fcc6a3da8eefbe2e6de8923da0053da
Cristian <85373118+ElDallasplayer@users.noreply.github.com>
2024-08-18 17:41:21 -0300

    [ELIMINO] Elimino menubar.js por tener codigo duplicado

--- js/menubar.js
+++
@@ -1,6 +0,0 @@
-$(".nav-link").on("click", (e) =>{
-    e.stopImmediatePropagation();
-
-    let referencia = e.target.getAttribute("href");
-    InsertComponent("principal-container",referencia);
-})
